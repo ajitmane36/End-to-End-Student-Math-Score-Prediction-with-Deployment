63635964a2f28318b4276b279a1746706828e06a
Ajit Mane <ajitmane36@gmail.com>
2024-07-29 13:13:33 +0530

    Mlflow Logging

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from mlproject.exception import CustomException
 from mlproject.components.data_ingestion import DataIngestion
 from mlproject.components.data_ingestion import DataIngestionConfig
 from mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
+from mlproject.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 
 
@@ -20,16 +21,20 @@ if __name__ == "__main__":
     
     try:
         
+        ## Data Ingestion
         #data_ingestion_config=DataIngestionConfig()
         data_ingestion=DataIngestion()
         train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
-
+        
+        ## Data Transformation
         #data_transformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
         train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
         
+        ## Model Traning
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+        
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e, sys)
-
-# Now we can see error in log files 
